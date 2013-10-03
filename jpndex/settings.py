8dd0f72ad8858ca700146cb793d14369eba65f1e
Leonard Witzel <witzel@post.harvard.edu>
2013-10-03 17:29:12 -0400

    heroku test

--- jpndex/settings.py
+++ jpndex/settings.py
@@ -174,14 +174,35 @@ LOGGING = {
 # ===================
 # = HEROKU SETTINGS =
 # ===================
-
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 ALLOWED_HOSTS = ['*']
+
 
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# ALLOWED_HOSTS = ['*']
+# 
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # STATIC_ROOT = 'public'
 # STATIC_URL = '/public/'
 # 
