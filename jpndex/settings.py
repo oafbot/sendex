9d5352dd65cffe141da9b076e04367669de35735
Leonard Witzel <witzel@post.harvard.edu>
2013-10-03 17:38:44 -0400

    heroku test

--- jpndex/settings.py
+++ jpndex/settings.py
@@ -187,12 +187,12 @@ ALLOWED_HOSTS = ['*']
 
 # import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+# 
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 
